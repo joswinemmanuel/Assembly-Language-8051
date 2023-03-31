b6420fc26a007935f82c4105a735c0c23d4a0d1e
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2023-03-31 21:20:33 +0530

    Division

---
+++ Division.asm
@@ -0,0 +1,4 @@
+MOV A, R1 ; Move the value in R1 to the accumulator
+MOV B, R2 ; Move the value in R2 to the B register
+DIV AB ; Divide the value in AB by the value in B and store the quotient in A and the remainder in B
+MOV R1, A ; Move the quotient back to R1
