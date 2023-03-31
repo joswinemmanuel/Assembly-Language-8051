d4910491d1dc20a2660c1c3816e6f190a1710e9e
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2023-03-31 21:19:40 +0530

    Multiplication

---
+++ Multiplication.asm
@@ -0,0 +1,4 @@
+MOV A, R1 ; Move the value in R1 to the accumulator
+MOV B, R2 ; Move the value in R2 to the B register
+MUL AB ; Multiply the value in A with the value in B and store the result in AB
+MOV R1, A ; Move the result back to R1
